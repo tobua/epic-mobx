33ae1dd034a7b147ccb3f2259fb22b24fad0444a
Matthias Giger <i@matthiasgiger.com>
2021-06-24 19:15:44 +0200

    feat(setup): quick implementation using observable

--- index.ts
+++ index.ts
@@ -1,7 +1,24 @@
-import { createAtom } from 'mobx'
+import { observable, IObservableArray } from 'mobx'
 
-export const nest = createAtom(
-  'Nest',
-  () => console.log('start observing'),
-  () => console.log('stop observing')
-)
+type Class = { new (...args: any[]): any }
+
+interface INestedObservableArray<T> extends IObservableArray<T> {
+  extend: (value: T) => void
+}
+
+export const nestable = <T>(initial: T[], Type: Class) => {
+  const wrapper = observable(initial)
+
+  if (
+    process.env.NODE_ENV !== 'production' &&
+    !(typeof Type === 'function' && /^\s*class\s+/.test(Type.toString()))
+  ) {
+    console.warn('Type needs to be a class.')
+  }
+
+  ;(wrapper as INestedObservableArray<T>).extend = (value: T) => {
+    wrapper.push(new Type(value))
+  }
+
+  return wrapper as INestedObservableArray<T>
+}
--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,10 +1,24 @@
 import { makeAutoObservable, autorun } from 'mobx'
-import { nest } from '../index'
+import { nestable } from '../index'
+
+class NestedClass {
+  count = 1
+
+  constructor(value: number) {
+    this.count = value
+  }
+
+  double() {
+    this.count *= 2
+  }
+}
 
 class StoreClass {
   count = 0
+  list = null
 
-  constructor() {
+  constructor(value: any, type: any) {
+    this.list = nestable(value, type)
     makeAutoObservable(this, {}, { autoBind: true })
   }
 
@@ -13,17 +27,31 @@ class StoreClass {
   }
 }
 
-const Store = new StoreClass()
+const consoleWarnMock = jest.fn()
+console.warn = consoleWarnMock
 
-const countMock = jest.fn(() => {
-  console.log(Store.count)
-})
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const noop = (...values: any[]) => {}
 
-const dispose = autorun(countMock)
+let Store
+let countMock
+let dispose
 
-afterAll(dispose)
+const createStore = (value: any, type: any) => {
+  Store = new StoreClass(value, type)
 
-test('Basic example.', () => {
+  countMock = jest.fn(() => {
+    noop(Store.count)
+  })
+
+  dispose = autorun(countMock)
+}
+
+afterEach(() => dispose())
+
+test('Basic store works and export is defined.', () => {
+  createStore([1, 2], NestedClass)
+
   expect(Store.count).toEqual(0)
   expect(countMock.mock.calls.length).toEqual(1)
 
@@ -32,5 +60,14 @@ test('Basic example.', () => {
   expect(Store.count).toEqual(2)
   expect(countMock.mock.calls.length).toEqual(2)
 
-  expect(nest).toBeDefined()
+  expect(nestable).toBeDefined()
+})
+
+test('Warning if second argument is not a class.', () => {
+  expect(consoleWarnMock.mock.calls.length).toEqual(0)
+
+  createStore([1, 2], {})
+
+  expect(consoleWarnMock.mock.calls.length).toEqual(1)
+  expect(consoleWarnMock.mock.calls[0][0]).toEqual('Type needs to be a class.')
 })
